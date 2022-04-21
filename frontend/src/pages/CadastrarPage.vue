cb94cf8b7cec25435dcebad80e0a95b982c5a93c
Gabriel R Pacheco <gabrielrodriguespacheco2@gmail.com>
2022-04-21 14:04:24 -0300

    Adicionando front de cadastraar

---
+++ frontend/src/pages/CadastrarPage.vue
@@ -0,0 +1,8 @@
+<script setup>
+import CadastroCard from 'src/components/CadastroCard.vue'
+</script>
+<template>
+<div class="bg-secondary row items-center justify-center">
+    <CadastroCard class="col-11 col-sm-8 col-md-6 col-lg-4 col-xl-4"/>
+</div>
+</template>
