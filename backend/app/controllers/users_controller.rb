d25d72a10175efaa282c34894a7e2ec0ee50a173
Gabriel R Pacheco <gabrielrodriguespacheco2@gmail.com>
2022-05-01 17:10:38 -0300

    Corrigindo erro de status e adicionando documentac

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
-##classe da controler do cadastro de usuarios
+##
+# Classe da controler do cadastro de usuarios
 class UsersController < ApplicationController
+  ##
+  # Metodo respond_with
+  # 
+  # Responsavel por decidir qual qual valor de user retornar e qual mensagem deve ser apresentada ao usuario.
+  # Dependnedo do resultado ela chama o register_failed ou o register_success 
+  #
+  # Parametros: resource, que representa o user que esta sendo tratado
+  #
+  # Retorno: respond_to :json
+  #
+  # Transformacao: Nenhuma
+  #
   respond_to :json
   private
   def respond_with(resource)
@@ -7,18 +20,55 @@ class UsersController < ApplicationController
 
     register_failed(resource)
   end
+  
+  ##
+  # Metodo register_success
+  # 
+  # Responsavel por retornar os dados de um registro bem  sucedido
+  #
+  # Parametros: resource, que representa o user que esta sendo tratado
+  #
+  # Retorno: json para o front end
+  #
+  # Transformacao: Nenhuma
+  #
   def register_success(resource)
     render json: {
       message: 'Cadastrado com sucesso',
       user: resource
     }, status: :ok
   end
+  
+  ##
+  # Metodo register_failed
+  # 
+  # Responsavel por retornar os dados de um registro mal  sucedido
+  #
+  # Parametros: resource, que representa o user que esta sendo tratado
+  #
+  # Retorno: json para o front end
+  #
+  # Transformacao: Nenhuma
+  #
   def register_failed(resource)
     render json: {
       message: 'Error ao cadastrar usuario',
       user: resource
-    }, status: :unauthorized
+    }, status: :ok
   end
+  
+  ##
+  # Construtor da classe
+  # 
+  # Recebe os dados para realizar a consulta no banco de dados. Caso exista o membro, mas
+  # nao um usuario ele cria um usuario para o membro no banco de dados
+  #
+  # Parametros: Dados de senha e email recebidos do front end
+  #
+  # Retorno: user
+  #
+  # Transformacao: Cria um user para um member no ba
+  #
   public
   def create
       email = params[:email_pessoa]
